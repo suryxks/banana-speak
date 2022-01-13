c7277d0e91e4945b46410a73b9935279be398586
Surya <123004250@sastra.ac.in>
2022-01-13 18:29:26 +0530

    refactor: upgraded to es6+

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-var btn=document.querySelector("#btn-translate")
-var outDiv=document.querySelector("#txt-output")
-var inputTextArea=document.querySelector("#txt-input");
-var url="https://api.funtranslations.com/translate/minion.json";
+var btn = document.querySelector("#btn-translate")
+var outDiv = document.querySelector("#txt-output")
+var inputTextArea = document.querySelector("#txt-input");
+var url = "https://api.funtranslations.com/translate/minion.json";
 
-function constructURL(text){
-    return url + "?" + "text=" + text;
+const constructURL = text => `${url}?text=${text}`;
+
+const errorHandler = error => {
+  alert("you Have crossed the limit Try after an hour", error);
 }
- function errorHandler(error){
-   alert("you Have crossed the limit Try after an hour",error);
- }
-btn.addEventListener("click", function(){
-  var txtval=inputTextArea.value;
- var op= fetch(constructURL(txtval))
-  .then(res=>res.json())
-  .then(json=>{
-      var output=json.contents.translated;
-      outDiv.innerText=output;
-      
-  })
-  .catch(errorHandler);
+btn.addEventListener("click", () => {
+  let txtval = inputTextArea.value;
+  let op = fetch(constructURL(txtval))
+    .then(res => res.json())
+    .then(json => {
+      var output = json.contents.translated;
+      outDiv.innerText = output;
+
+    })
+    .catch(errorHandler);
 
 });
